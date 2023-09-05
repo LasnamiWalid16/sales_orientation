1f20a33c22497e13bfa2dd2d3f7ff50b885edd0f
Lasnami Abdelkarim <81482604+LasnamiWalid16@users.noreply.github.com>
2023-09-05 16:05:52 +0100

    Add submit button

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from google.cloud import bigquery
 st.title("SAG Student Orientation")
 
 # Add some text
-st.write("Choose your best course in few clicks !")
-
+st.write("Choose your best course in a few clicks!")
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
@@ -18,38 +17,44 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 
 project_id = 'ga4bigquery-364118'
-client = bigquery.Client(credentials=credentials,project=project_id)
-
+client = bigquery.Client(credentials=credentials, project=project_id)
 
 @st.cache_data(ttl=600)
 def run_query(query):
-    select = client.query(query)
+    select = client.query(query, job_config=job_config)
     return select.to_dataframe()
-  
 
-all_data = run_query(" SELECT * FROM `ga4bigquery-364118.mongoDB.all_data` ")
-all_data['fees'] = pd.to_numeric(all_data['fees'], errors='coerce').astype('float')
-
+# Create a Streamlit selectbox to choose a localCountry
+selected_localCountryName = st.selectbox("Select a local Country", ['Algeria', 'Nigeria', 'Morocco'])
 
 # Create a Streamlit selectbox to choose a degree
-selected_degree = st.selectbox("Select a degree", ['Bachelor','Master'])
+selected_degree = st.selectbox("Select a degree", ['Bachelor', 'Master', 'Phd'])
 
-# Create a Streamlit selectbox to choose a admissionType
-selected_admissionType = st.selectbox("Select a admissionType", ['direct','pathway','both'])
+# Create a Streamlit selectbox to choose a discipline
+selected_discipline = st.selectbox("Select a discipline", ['Professions and Applied Sciences', 'Social Science', 'Art',
+                                                           'Humanities', 'Formal Sciences', 'Natural Sciences'])
 
-# Create a Streamlit selectbox to choose a university
-selected_university = st.selectbox("Select a University", all_data['university'].unique())
+# Create a Streamlit selectbox to choose an admissionType
+selected_admissionType = st.selectbox("Select an admissionType", ['pathway', 'direct', 'both'])
 
-selected_intake = st.multiselect("Select Intake", all_data['intakeName'].unique(), default=all_data['intakeName'].unique())
+# Create a Streamlit selectbox to choose a country
+selected_country = st.selectbox("Select a country you want to apply for", ['England', 'Northern Ireland', 'Scotland', 'Australia', 'Wales', 'Ireland', 'United States', 'Netherlands'])
 
+# Create a submit button
+if st.button("Show Results"):
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("degree", "STRING", selected_degree),
+        ]
+    )
 
-# Filter the DataFrame based on the selected university
-filtered_data = all_data[(all_data['university'] == selected_university) & (all_data['degree'] == selected_degree)
-                         & (all_data['admissionType'] == selected_admissionType) & all_data['intakeName'].isin(selected_intake)]
+    sql_query = f""" SELECT * FROM `ga4bigquery-364118.mongoDB.all_data` 
+                     WHERE localCountryName = '{selected_localCountryName}'  
+                     AND   degree = '{selected_degree}'  
+                     AND   discipline = '{selected_discipline}'  
+                     AND   admissionType = '{selected_admissionType}'  
+                     AND   country = '{selected_country}'   """
+                     
+    all_data = run_query(sql_query)
 
-# Display the filtered data
-st.write(f"Data for students from {selected_university}:")
-st.write(filtered_data)
-
-
-
+    st.write(all_data)
